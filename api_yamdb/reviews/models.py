ff7541bf5a6f733baa5975051101becbf15ed71e
Pavel Kozlov <skarukzor@gmail.com>
2022-12-02 19:32:40 +0300

    done models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=96
+    )
+    slug = models.SlugField(
+        max_length=96,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=96
+    )
+    slug = models.SlugField(
+        max_length=96,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=96
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Год'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        max_length=192,
+        null=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        blank=True,
+        related_name='titles',
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
