d771c268887ff06cf3ac5e07a14148242508e7c9
Pavel Kozlov <skarukzor@gmail.com>
2022-12-03 14:59:51 +0300

    fixed fitlering and searching

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,5 @@
-from rest_framework.filters import SearchFilter
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from reviews.models import Category, Genre, Title
 from .mixins import CreateListDestroyViewSet
 from .permissions import IsAdminOrReadOnly
@@ -14,18 +14,23 @@ class CategoryViewSet(CreateListDestroyViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
 
 
 class GenreViewSet(CreateListDestroyViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     permission_classes = (IsAdminOrReadOnly,)
-    filter_backends = (SearchFilter,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category', 'genre', 'name', 'year')
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
