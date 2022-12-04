eb90d3f6c7d63dc9b053cb131a18c9fe89905482
Pavel Kozlov <skarukzor@gmail.com>
2022-12-04 17:31:14 +0300

    small fixes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -85,8 +85,7 @@ class TitleViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    # добавить ограничение на кол-во постов
-    permission_classes = [IsAuthenticatedOrReadOnly & IsAdminOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
@@ -97,17 +96,18 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
         user_comments = title.reviews.filter(user=self.request.user)
-        if user_comments.objects.count() < 1:
-            serializer.save(
-                # Не уверен насчет 'or self.request.user.is_admin'
-                author=self.request.user or self.request.user.is_admin, title_id=self.kwargs.get('post_id')
-            )
+        if user_comments.objects.count() > 0:
+            raise ValueError
+        serializer.save(
+            author=self.request.user,
+            title_id=self.kwargs.get('title_id')
+        )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly & IsAdminOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_queryset(self):
         review_id = self.kwargs.get('review_id')
@@ -116,6 +116,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(
-            # Не уверен насчет 'or self.request.user.is_admin'
-            author=self.request.user or self.request.user.is_admin, review_id=self.kwargs.get('post_id')
+            author=self.request.user,
+            review_id=self.kwargs.get('review_id')
         )
